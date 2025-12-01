aa81a90cf2ad3a32a810715b916bb46b434d5edb
AnythingJR <mauriciojunior2209@gmail.com>
2025-12-01 19:16:23 -0300

    Criação do POST e GET/Teste de funcionamento

--- server.js
+++ server.js
@@ -5,15 +5,67 @@ const app = express()
 /*utilizando JSON*/
 app.use(express.json())
 
-const order = []
+const orders = []
 
 /*criando pedido*/
-app.post('/order', (req, res) => {
+app.post('/orders', (req, res) => {
+    try {
+        const { numeroPedido, valorTotal, dataCriacao, items } = req.body
 
-    order.push(req.body)
+        //Validação básica
+        if (!numeroPedido || !valorTotal || !dataCriacao || !items) {
+            return res.status(400).json({
+                error: 'Dados obrigatórios não informados.'
+            })
+        }
 
+        //Fazendo o mapping (transformação)
+        const mappedOrder = {
+            orderId: numeroPedido,
+            value: valorTotal,
+            creationDate: new Date(dataCriacao),
+            items: items.map(item => ({
+                productId: Number(item.idItem),
+                quantity: item.quantidadeItem,
+                price: item.valorItem
+            }))
+        }
 
-    res.send ('pedido criado com sucesso!')
+        //Salvando temporariamente em memória
+        orders.push(mappedOrder)
+
+        //Retorno correto
+        return res.status(201).json({
+            message: 'Pedido criado com sucesso',
+            order: mappedOrder
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            error: 'Erro interno no servidor'
+        })
+    }
 })
 
-app.listen(3000)
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`)
+})
+
+
+app.get('/order/:id', (req, res) => {
+    const { id } = req.params
+
+    // Procurando pedido pelo ID
+    const orderFound = orders.find(order => order.orderId === id)
+
+    // Se não encontrar
+    if (!orderFound) {
+        return res.status(404).json({
+            error: 'Pedido não encontrado'
+        })
+    }
+
+    // Se encontrar
+    return res.status(200).json(orderFound)
+})
